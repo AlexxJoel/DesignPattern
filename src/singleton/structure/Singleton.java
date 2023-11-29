d5ce27f434fdae9cb286fe35d88dd87ee7a9e58d
AlexxJoel <20213tn019@utez.edu.mx>
2023-11-29 01:17:33 -0600

    [ADD] singleton design pattern

--- src/singleton/structure/Singleton.java
+++ src/singleton/structure/Singleton.java
@@ -4,11 +4,7 @@ public class Singleton {
     private static Singleton instance;
     private Singleton() {}
     public static Singleton getInstance() {
-        if (instance == null) {
-            instance = new Singleton();
-        }
+        if (instance == null) instance = new Singleton();
         return instance;
     }
-
-
 }
